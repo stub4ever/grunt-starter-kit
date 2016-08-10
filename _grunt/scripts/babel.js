5c32048dfa362bf6779b4fb49191ab269d02f650
Quoc bui <q.bui@caesar.nl>
2016-08-10 16:11:18 +0200

    update babel task

--- _grunt/scripts/babel.js
+++ _grunt/scripts/babel.js
@@ -12,12 +12,13 @@ module.exports.tasks = {
    // Compiles ES6 with Babel
     babel: {
       options: {
-        sourceMap: true
+        sourceMap: true,
+        presets: ['babel-preset-es2015'],
       },
       dist: {
         files: [{
           expand: true,
-          cwd: '/<%=config.js.srcDir%>',
+          cwd: '<%=config.js.srcDir%>',
           src: '{,*/}*.js',
           dest: '<%=config.js.tempDir%>',
           ext: '.js'
